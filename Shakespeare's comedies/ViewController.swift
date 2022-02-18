529c93ac0eed66a33a6f78dac9f51f71679269db
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-02-18 17:11:30 +0200

    create tableview and disply data

--- Shakespeare's comedies/ViewController.swift
+++ Shakespeare's comedies/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Shakespear's Comidies"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
     }
+}
 
-
+//MARK: - Table View
+extension ViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // configure cell...
+        cell.textLabel?.text = String(indexPath.row)
+        cell.detailTextLabel?.text = String(indexPath.section)
+        
+        return cell
+    }
+    
 }
 
